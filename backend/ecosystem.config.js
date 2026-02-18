9565ab154d9e58add758545cb7e8e278ad2008b1
Krishi_NIELIT <krishisahayogi.2025@gmail.com>
2026-02-18 12:25:23 +0530

    port and path fixed

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -2,16 +2,16 @@ module.exports = {
   apps: [
     {
       name: "SysLoggerBackend",
-      script: "/Users/ayushranjan/Sys_Logger/backend/sys_logger.py",
-      cwd: "/Users/ayushranjan/Sys_Logger/backend",
-      interpreter: "/Users/ayushranjan/Sys_Logger/backend/venv/bin/python3",
+      script: "sys_logger.py",
+      cwd: "./",
+      interpreter: "./venv/Scripts/python.exe",
       instances: 1,
       autorestart: true,
       watch: false,
       max_memory_restart: "500M",
       env: {
         PYTHONUNBUFFERED: "1",
-        PORT: "5001"
+        PORT: "5010"
       },
       error_file: "./backend_err.log",
       out_file: "./backend_out.log",
